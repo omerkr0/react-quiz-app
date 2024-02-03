cce5d50e43b71302cb5e94c73585f8c42d9d5089
omerkr0 <omerkaramanx@gmail.com>
2024-02-03 18:57:59 +0300

    Home page edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="my-4 text-xl">Welcome to Quiz</h1>
-      <Link to="/quiz">
-        <button className="text-xl bg-blue-400 hover:bg-blue-500 py-2 px-4 rounded-md" type="button">Start Quiz</button>
+    <div className="flex flex-col items-center justify-center h-screen font-semibold">
+      <h1 className="text-2xl sm:text-4xl mb-5">Quiz&apos;e Hoşgeldiniz!</h1>
+      <Link
+        to="/quiz"
+        className="text-sm sm:text-lg bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-xl"
+      >
+        Quiz&apos;i Başlat
       </Link>
     </div>
   );
