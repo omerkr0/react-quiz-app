45ed276bf88d92f40aa5daaed5f8d68fd084996b
omerkr0 <omerkaramanx@gmail.com>
2024-02-03 19:02:30 +0300

    QuizData added

---
+++ src/data/QuizData.js
@@ -0,0 +1,34 @@
+const quizData = [
+  {
+    id: 1,
+    text: "Hangisi frontend kapsamında değerlendirilmez?",
+    options: [
+      { id: 1, text: "Html", isCorrect: false },
+      { id: 2, text: "Css", isCorrect: false },
+      { id: 3, text: "JavaScript", isCorrect: false },
+      { id: 4, text: "Sql", isCorrect: true },
+    ],
+  },
+  {
+    id: 2,
+    text: "Hangisi backend kapsamında değerlendirilir?",
+    options: [
+      { id: 1, text: "Node.js", isCorrect: true },
+      { id: 2, text: "Typescript", isCorrect: false },
+      { id: 3, text: "Angular", isCorrect: false },
+      { id: 4, text: "React", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    text: "Hangisi javascript programlama dilini kullanmaz?",
+    options: [
+      { id: 1, text: "React", isCorrect: false },
+      { id: 2, text: "Angular", isCorrect: false },
+      { id: 3, text: "Vue.js", isCorrect: false },
+      { id: 4, text: ".Net", isCorrect: true },
+    ],
+  },
+];
+
+export default quizData;
